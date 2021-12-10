c92a52552e465e7712a1d8ce4bdff74d5e68c2ac
Aurelien CZN <aurelienczn02@gmail.com>
2021-12-10 15:13:47 +0100

    maj

--- nav.js
+++ nav.js
@@ -3,7 +3,11 @@ let nav = document.getElementById("nav");
 let btn = document.getElementById("btn");
 btn.addEventListener("click", affichageNav);
 
+let header = document.getElementById("header")
+
 let links = document.querySelectorAll(".link");
+
+let main = document.getElementById("anime")
 
 links.forEach((link) => {
   console.log(link);
@@ -17,10 +21,18 @@ function affichageNav() {
     nav.style.visibility = "visible";
     nav.style.opacity = "1";
     nav.style.zIndex = "9998";
+    nav.style.width = "15vw";
+    header.style.width = "15vw";
+    main.style.width = "90vw"
+    main.style.marginLeft = "auto"
+
   } else {
     nav.style.visibility = "hidden";
     nav.style.opacity = "0";
     nav.style.zIndex = "-9998";
+    header.style.width = "0vw";
+    nav.style.width = "0%"
+    main.style.width = "100vw"
   }
 }
 
